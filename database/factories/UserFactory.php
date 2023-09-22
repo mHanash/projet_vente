b75b1e2ce51cc06855fa63b1f3965eaa66ae156d
mHanash <zenasmomonzo@gmail.com>
2023-09-22 09:14:15 +0100

    composer update

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,8 +18,9 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'firtname' => $this->faker->name(),
+            'firstname' => $this->faker->name(),
             'lastname' => $this->faker->name(),
+            'phone' => $this->faker->phoneNumber(),
             'email' => 'admin@admin.com',
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
